6db18aea5331f18ce5be0ff67b15fa9b917cd260
franklin Perez <franklin.perez@dilimport.com>
2024-10-18 17:53:09 -0600

    SubiendoTarea
    Commit para subir tarea

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using EjercicioClase;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SuperHeroe superman = new SuperHeroe();
+        superman.Nombre = "Superman";
+        superman.IdentidadSecreta = "Clark Kent";
+        superman.Ciudad = "Metropolis";
+        superman.PuedeVolar = true;
+        superman.SuperPoder.Nombre = "Super Fuerza";
+        superman.SuperPoder.Descripcion = "Capacidad de levantar objetos muy pesados";
+        superman.SuperPoder.Nivel = 9;
+        superman.Imprimir();
+
+        SuperHeroe invisibleWoman = new SuperHeroe();
+        invisibleWoman.Nombre = "Invisible Woman";
+        invisibleWoman.IdentidadSecreta = "Sue Storm";
+        invisibleWoman.Ciudad = "New York";
+        invisibleWoman.PuedeVolar = false;
+        invisibleWoman.SuperPoder.Nombre = "Invisibilidad";
+        invisibleWoman.SuperPoder.Descripcion = "Habilidad para volverse invisible";
+        invisibleWoman.SuperPoder.Nivel = 8;
+        invisibleWoman.Imprimir();
+
+        SuperHeroe nightcrawler = new SuperHeroe();
+        nightcrawler.Nombre = "Batman";
+        nightcrawler.IdentidadSecreta = "Bruno Diaz";
+        nightcrawler.Ciudad = "Ciudad Gotica";
+        nightcrawler.PuedeVolar = false;
+        nightcrawler.SuperPoder.Nombre = "Millonario";
+        nightcrawler.SuperPoder.Descripcion = "Usa tecnologia";
+        nightcrawler.SuperPoder.Nivel = 1;
+        nightcrawler.Imprimir();
+    }
+}
--- EjercicioClase/SuperHeroe.cs
+++ EjercicioClase/SuperHeroe.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EjercicioClase
+﻿namespace EjercicioClase
 {
-    internal class SuperHeroe
+    public class SuperHeroe
     {
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public bool PuedeVolar { get; set; }
+        public SuperPoder SuperPoder { get; set; }
+
+        public SuperHeroe()
+        {
+            SuperPoder = new SuperPoder();
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede volar: {PuedeVolar.ToString().ToLower()}");
+            Console.WriteLine($"Super Poder: {SuperPoder.Nombre}");
+            Console.WriteLine($"Descripción: {SuperPoder.Descripcion}");
+            Console.WriteLine($"Nivel: {SuperPoder.Nivel}");
+            Console.WriteLine();
+        }
     }
 }
